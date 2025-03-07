6022a15c0afa8b7153eab0fce99b1ae478240902
rushii <rushitalkar@gmail.com>
2025-03-07 17:45:24 +0530

    practse
    practise

---
+++ CHATAPP/backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+const login =(req,res)=>{
+    res.send("login")
+}
+
+const signup=(req,res)=>{
+    res.send("signup")    
+}
+const logout=(req,res)=>{
+    res.send("logout")
+}
+export {login,signup,logout}
---
+++ CHATAPP/backend/src/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {login,signup,logout} from "../controllers/auth.controllers.js"
+const router = express.Router()
+
+router.get("/signup" , signup) 
+router.get("/login" , login)
+router.get("/logout" , logout)
+export default router
